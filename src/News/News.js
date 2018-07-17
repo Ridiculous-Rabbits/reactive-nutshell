db30ce349ebf318412c7fb25f1626b731b9d0c57
William Kimball <wkkimball043@gmail.com>
2018-07-17 14:34:25 -0500

    copied news structure over

--- src/News/News.js
+++ src/News/News.js
@@ -3,35 +3,35 @@ import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 
 
-const Animal = ({ animal, children }) => {
+const News = ({ news, children }) => {
     return (
         <div className="card" style={{width: `18rem`}}>
             <div className="card-body">
                 <h5 className="card-title">
-                    {animal.name}
+                    {news.name}
                 </h5>
-                <p className="card-text">{animal.breed}</p>
+                <p className="card-text">{news.breed}</p>
                 {
                     <Link className="card-link"
                     to={{
-                        pathname: `/animals/${animal.id}`,
-                        state: { animal: animal }
+                        pathname: `/newss/${news.id}`,
+                        state: { news: news }
                     }}>
                         Details
                     </Link>
                 }
-                <a href="#" onClick={() => this.props.checkOutAnimal(animal.id)}>Delete</a>
+                <a href="#" onClick={() => this.props.checkOutNews(news.id)}>Delete</a>
             </div>
         </div>
     )
 }
 
-Animal.propTypes = {
-  // This rule ensures that `animal` is passed a property
+News.propTypes = {
+  // This rule ensures that `news` is passed a property
   // and that is an object - not a string or number
-  animal: PropTypes.object.isRequired,
+  news: PropTypes.object.isRequired,
   id: PropTypes.number,
   name: PropTypes.string
 }
 
-export default Animal
+export default News

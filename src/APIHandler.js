76e90f296decb997c5f28f839206430271e28684
Natasha Cox <natashac1014@gmail.com>
2018-07-17 14:58:54 -0500

    added API handler

---
+++ src/APIHandler.js
@@ -0,0 +1,31 @@
+
+export default class APIHandler {
+    static getData = (section) => {
+        return fetch(`http://localhost:5002/${section}`)
+        .then(e => e.json())
+    }
+    static editData = (section, id, body) => {
+        return fetch(`http://localhost:5002/${section}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+        
+    }
+    static deleteData = (section, id) => {
+        return fetch(`http://localhost:5002/${section}/${id}`, {
+            method: "DELETE"
+        })
+    }
+    static addData = (section, id, body) => {
+        return fetch(`http://localhost:5002/${section}/${id}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify(body)
+        })
+    }
+}

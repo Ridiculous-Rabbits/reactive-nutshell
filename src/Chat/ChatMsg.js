41b9f54c02a3d017b2a30114b3f7ccc897d30747
Natasha Cox <natashac1014@gmail.com>
2018-07-23 14:29:37 -0500

    after a few hours, we made things workgit add --all

--- src/Chat/ChatMsg.js
+++ src/Chat/ChatMsg.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import "./ChatStyle.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import APIHandler from "../APIHandler"
+import AddFriendFunc from "../AddFriendFunc"
 
 
 
@@ -14,13 +15,37 @@ export default props => {
 // Check if the data is in `props.animal`
 if (props.hasOwnProperty("chatMsg")) {
     message = props.chatMsg
-
-// If not, data will be in `props.location.state.message`
+    
+    // If not, data will be in `props.location.state.message`
 } else {
     message = props.location.state.chatMsg
 }
-
 let currentUser = JSON.parse(sessionStorage.getItem("credentials"))
+let yourId = currentUser.userId
+let addNewFriend = () => {
+    APIHandler.getData("users")
+                .then((userArray) => {
+
+                    userArray.forEach(userObject => {
+                        if (userObject.name === message.user.name) {
+                            fetch(`http://localhost:5002/friends`, {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json; charset=utf-8"
+                                },
+                                body: JSON.stringify({
+                                    userId: userObject.id,
+                                    yourId: yourId
+                                })
+                            }).then(() => {
+
+                            })
+                        }
+                    })
+                })
+        }
+
+
 console.log("message.userId", message.userId)
 console.log(currentUser.userId)
 if (message.userId == currentUser.userId) {
@@ -31,7 +56,7 @@ if (message.userId == currentUser.userId) {
 return (
         <div className="card" style={{width: `18rem`}}>
             <div className="card-body">
-                <button className={message.user.name}>
+                <button onClick={addNewFriend} id={message.user.name} className={message.user.name}>
                 {message.user.name}:
                 </button>
                 <h3 className={message.className}>

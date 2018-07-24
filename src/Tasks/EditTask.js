0aeb958da26d097c36e37d06b7937f2447d64821
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-07-24 10:13:56 -0500

    starting adding notes to my task files

--- src/Tasks/EditTask.js
+++ src/Tasks/EditTask.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import APIHandler from "../APIHandler";
 
-class EditTask extends Component {
+export default class EditTask extends Component {
   state = {
     task: this.props.task.task,
     date: this.props.task.date
   };
 
+  // onChange - capture change in data user is entering in input fields. called within this file
   handleFieldChange = e => {
     const stateToChange = {};
-    stateToChange[e.target.id] = e.target.value;
-    this.setState(stateToChange);
+    stateToChange[e.target.id] = e.target.value; // adds values from input fields into empty object
+    this.setState(stateToChange); // updates database with the new data that has been passed in via stateToChange object
   };
 
+  // onSubmit - edit task data and re-render DOM
   handleUpdate = e => {
-    e.preventDefault();
+    e.preventDefault(); // stops default action of form reloading
 
+    // when editing data, you need to include all keys in the data table, even if you are not going to edit all of the data values
     const updatedTask = {
       task: this.state.task,
       date: this.state.date,
@@ -23,8 +26,8 @@ class EditTask extends Component {
       completed: this.props.task.completed
     };
 
-    APIHandler.editData("tasks", this.props.task.id, updatedTask).then(() => {
-      this.props.history.push("/tasks");
+    APIHandler.editData("tasks", this.props.task.id, updatedTask).then(() => { // passing in updatedTask object as an argument
+      this.props.history.push("/tasks"); // what's going on here?
     });
   };
 
@@ -62,4 +65,4 @@ class EditTask extends Component {
   }
 }
 
-export default EditTask // exports to ApplicationViews.js
+// exports to ApplicationViews.js
